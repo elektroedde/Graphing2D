862b4f4e84e71811b031b72c4f648d14a7025592
elektroedde <edvinberling@gmail.com>
2026-03-02 00:17:03 +0100

    refactor

--- elektroengine/C++/meshing.cpp
+++ elektroengine/C++/meshing.cpp
@@ -1,88 +1,11 @@
-//
-//  Hello.cpp
-//  Simple C++ example file
-//
-
 #include "meshing.hpp"
 
 
-void createRectangle() {
-    gmsh::initialize();
-
-    gmsh::model::occ::addRectangle(0, 0, 0, 3, 3);
-
-    gmsh::model::occ::synchronize();
-
-    gmsh::model::mesh::generate(2);
-
-    gmsh::finalize();
-
-
-}
-std::vector<double> printHello() {
-    gmsh::initialize();
-
-    gmsh::model::add("t1");
-
-    double lc = 1e-2;
-    gmsh::model::geo::addPoint(0, 0, 0, lc, 1);
-
-
-    gmsh::model::geo::addPoint(.1, 0, 0, lc, 2);
-    gmsh::model::geo::addPoint(.1, .3, 0, lc, 3);
-
-
-    int p4 = gmsh::model::geo::addPoint(0, .3, 0, lc);
-
-
-    gmsh::model::geo::addLine(1, 2, 1);
-    gmsh::model::geo::addLine(3, 2, 2);
-    gmsh::model::geo::addLine(3, p4, 3);
-    gmsh::model::geo::addLine(4, 1, p4);
-
-
-    gmsh::model::geo::addCurveLoop({4, 1, -2, 3}, 1);
-
-
-    gmsh::model::geo::addPlaneSurface({1}, 1);
-
-
-    gmsh::model::geo::synchronize();
-
-
-    gmsh::model::addPhysicalGroup(1, {1, 2, 4}, 5);
-    gmsh::model::addPhysicalGroup(2, {1}, 6, "My surface");
-
-
-    gmsh::model::mesh::generate(2);
-
-
-    gmsh::write("t1.msh");
-
-
-
-    std::vector<std::size_t> nodeTags;
-    std::vector<double> coord;
-    
-    gmsh::model::mesh::getNodesForPhysicalGroup(2, 6, nodeTags, coord);
-
-    // Reshape into n x 3 array (each row is [x, y, z])
-    std::vector<std::vector<double>> points;
-    for (size_t i = 0; i < coord.size(); i += 3) {
-        points.push_back({coord[i], coord[i+1], coord[i+2]});
-    }
-
-    gmsh::finalize();
-
-    // For now, return flattened coord - you'll need to update the header
-    return coord;
-
-}
 MeshData getMeshPoints() {
     gmsh::initialize();
     gmsh::model::add("t1");
 
-    double lc = 5e-1;
+    double lc = 1;
     gmsh::model::geo::addPoint(0, 0, 0, lc, 1);
     gmsh::model::geo::addPoint(10, 0, 0, lc, 2);
     gmsh::model::geo::addPoint(10, 10, 0, lc, 3);
@@ -110,52 +33,56 @@ MeshData getMeshPoints() {
     gmsh::write("t1.msh");
 
 
-
-    std::vector<std::size_t> eTags;
-    std::vector<std::size_t> nTags;
-
-
-    gmsh::model::mesh::getElementsByType(2, eTags, nTags);
-
-    std::vector<std::size_t> n_tag;
+    //Using these
+    std::vector<std::size_t> nodes;
     std::vector<double> n_coord;
-    std::vector<double> p_coord;
-
-    gmsh::model::mesh::getNodes(n_tag, n_coord, p_coord);
-
+    std::vector<std::vector<double>> nodeCoords;
     std::vector<std::size_t> physicalGroup1_nodes;
     std::vector<double> physicalGroup1_coords;
-
-
-    gmsh::model::mesh::getNodesForPhysicalGroup(1, 10, physicalGroup1_nodes, physicalGroup1_coords);
-
     std::vector<std::size_t> physicalGroup2_nodes;
     std::vector<double> physicalGroup2_coords;
+    std::vector<std::size_t> oneDimElements;
+    std::vector<std::size_t> oneDimNodeTags;
+
+
+
+
+
+
+
+
+
+
+    std::vector<std::size_t> eTags;
+    std::vector<std::size_t> n_tag;
+    std::vector<double> p_coord;
+
+
+    // Using these
+    gmsh::model::mesh::getElementsByType(2, eTags, nodes);
+    gmsh::model::mesh::getNodes(n_tag, n_coord, p_coord);
+    gmsh::model::mesh::getNodesForPhysicalGroup(1, 10, physicalGroup1_nodes, physicalGroup1_coords);
     gmsh::model::mesh::getNodesForPhysicalGroup(1, 11, physicalGroup2_nodes, physicalGroup2_coords);
-
-
-    std::vector<std::vector<double>> n_cord;
+    gmsh::model::mesh::getElementsByType(1, oneDimElements, oneDimNodeTags, 2);
+    
     for (size_t i = 0; i < n_coord.size(); i += 3) {
-        n_cord.push_back({n_coord[i], n_coord[i+1], n_coord[i+2]});
+        nodeCoords.push_back({n_coord[i], n_coord[i+1], n_coord[i+2]});
     }
-
-
-
     gmsh::finalize();
 
     MeshData data;
-    data.eTags = eTags;
-    data.nTags = nTags; //and this
-
-    data.n_tag = n_tag;
-    data.n_coord = n_coord;
-    data.p_coord = p_coord;
-    data.n_cord = n_cord; // Im using this
-
+    data.nodes = nodes;
+    data.nodeCoords = nodeCoords;
     data.physicalGroup1_nodes = physicalGroup1_nodes;
     data.physicalGroup1_coords = physicalGroup1_coords;
     data.physicalGroup2_nodes = physicalGroup2_nodes;
     data.physicalGroup2_coords = physicalGroup2_coords;
+    data.oneDimElements = oneDimElements;
+    data.oneDimNodeTags = oneDimNodeTags;
+
+
+
+
     return data;
 }
 
---
+++ elektroengine/Geometry/FEM.swift
@@ -0,0 +1,19 @@
+struct FEM_Model {
+
+    var N: Int { vertices.count }
+    var M: Int { nodes.count / 3 }
+
+    var nodes: [Int] = []
+    var vertices: [Vertex] = []
+
+    var dirichletNodes: [Int] = []
+    var dirichletValues: [Int] = []
+
+    var robinElements: [Int] = []
+    var robinNodes: [Int] = []
+
+    var f: [Float]? = nil
+    var q: [Float] = []
+    var gamma: [Float] = []
+
+}
