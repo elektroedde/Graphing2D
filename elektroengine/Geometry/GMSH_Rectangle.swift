de9eecec2c0296abed421b37ff685cef9c11d7dd
elektroedde <edvinberling@gmail.com>
2026-03-01 13:12:00 +0100

    s

--- elektroengine/Geometry/GMSH_Rectangle.swift
+++ elektroengine/Geometry/GMSH_Rectangle.swift
@@ -4,7 +4,7 @@ import Accelerate
 
 struct GMSH_Rectangle: Transformable {
 
-    //var hsitboxs: BoundingSphere = BoundingSphere(center: [0,0,0], radius: 1)
+    //var hsitbsoxs: BoundingSphere = BoundingSphere(center: [0,0,0], radius: 1)
 
 
     var pipelineState: MTLRenderPipelineState!
